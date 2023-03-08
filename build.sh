722f7a244243b51210dc5a9164a358babf483172
Ayush Sehrawat <scientificnationwords@gmail.com>
2023-03-08 22:23:25 +0530

    initial commit

---
+++ build.sh
@@ -0,0 +1,1 @@
+npx tailwindcss -i ./tailwind.css -o ./style.css --watch
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "exo-2": ["'Exo 2'", "sans-serif"],
+        lato: ["Lato", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
