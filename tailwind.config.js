3f94b70d92d37db52972898a4e054b09e219e1f6
Ayush Sehrawat <scientificnationwords@gmail.com>
2023-03-09 12:04:49 +0530

    Tweaks in meta tags, changes in color and fixing overflow x on mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        "div-bg": "#ffffff0f",
+        "hover-div-bg": "#ffffff1f",
+      },
       fontFamily: {
         "exo-2": ["'Exo 2'", "sans-serif"],
         lato: ["Lato", "sans-serif"],
