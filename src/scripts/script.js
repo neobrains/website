7fc899500de30b466bcd9b19aee46f32b383c26f
Ayush Sehrawat <itsayushsehrawat@gmail.com>
2023-03-11 22:39:39 +0530

    Converted to astro

---
+++ src/scripts/script.js
@@ -0,0 +1,22 @@
+let expand = document.getElementById("expand");
+let modal = document.getElementById("modal");
+let isExpanded = false;
+expand.addEventListener("click", () => {
+  if (isExpanded) {
+    expand.className = "fa-solid fa-bars";
+    isExpanded = false;
+    modal.style.display = "none";
+  } else {
+    expand.className = "fa-solid fa-xmark";
+    isExpanded = true;
+    modal.style.display = "flex";
+  }
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 768) {
+    isExpanded = false;
+    modal.style.display = "none";
+    expand.className = "fa-solid fa-bars";
+  }
+});
