0e586011ca2a1a2c574ca0ff6bb02acaa1593a9e
Ayush Sehrawat <itsayushsehrawat@gmail.com>
2023-03-11 22:46:32 +0530

    Switched to astro

--- build.sh
+++
@@ -1,1 +0,0 @@
-npx tailwindcss -i ./tailwind.css -o ./style.css --watch
---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+		  colors: {
+			"div-bg": "#ffffff0f",
+			"hover-div-bg": "#ffffff1f",
+		  },
+		  fontFamily: {
+			"exo-2": ["'Exo 2'", "sans-serif"],
+			lato: ["Lato", "sans-serif"],
+		  },
+		},
+	  },
+	plugins: [],
+}
