d91b6e298becff0b1c14909e1785c5e51e35574f
Ayush Sehrawat <itsayushsehrawat@gmail.com>
2023-03-12 15:42:14 +0530

    Better seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://neobrains.me',
+  integrations: [tailwind(), sitemap()]
 });
