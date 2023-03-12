316eb1644cbd3a4853c371cbc6f0044b52b87934
Ayush Sehrawat <itsayushsehrawat@gmail.com>
2023-03-12 19:58:34 +0530

    Added compression on build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
+import compress from "astro-compress";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://neobrains.me',
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), compress()]
 });
