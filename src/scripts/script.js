f20591157a6881f678c9801dd2d1b8e226b0fd93
Ayush Sehrawat <itsayushsehrawat@gmail.com>
2023-03-13 01:02:57 +0530

    Some effect added

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -19,3 +19,14 @@ window.addEventListener("resize", () => {
     modal.style.display = "none";
   }
 });
+
+document.getElementById("cards").onmousemove = e => {
+  for(const card of document.getElementsByClassName("card")) {
+    const rect = card.getBoundingClientRect(),
+          x = e.clientX - rect.left,
+          y = e.clientY - rect.top;
+
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  };
+}
