1926c5d9a1a0e1a1ecaaa5a8d5b8cefa11d994f2
Ayush Sehrawat <itsayushsehrawat@gmail.com>
2023-03-12 17:50:41 +0530

    Fixing some issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://neobrains.me',
   integrations: [tailwind(), sitemap()],
-  output: "server",
-  adapter: cloudflare()
 });
